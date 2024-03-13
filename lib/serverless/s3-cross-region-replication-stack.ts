d685eb5d7c1a31f7e8c09464bfbd35697e11bd28
hkstwk <hkolvoort@gmail.com>
2024-03-13 08:13:27 +0100

    added target bucket for replication

--- lib/serverless/s3-cross-region-replication-stack.ts
+++ lib/serverless/s3-cross-region-replication-stack.ts
@@ -1,23 +1,40 @@
 import * as cdk from 'aws-cdk-lib';
-import {aws_s3 as s3, RemovalPolicy, Duration} from 'aws-cdk-lib';
+import {Duration, RemovalPolicy} from 'aws-cdk-lib';
+import {BlockPublicAccess, Bucket, BucketEncryption, ObjectOwnership} from 'aws-cdk-lib/aws-s3';
 import {Construct} from 'constructs';
 
 export class S3CrossRegionReplicationStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
-        const sourceBucket = new s3.Bucket(this, 'sourceBucket', {
-            blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
-            encryption: s3.BucketEncryption.S3_MANAGED,
+        const sourceBucket = new Bucket(this, 'sourceBucket', {
+            blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+            encryption: BucketEncryption.S3_MANAGED,
             versioned: true,
             removalPolicy: RemovalPolicy.DESTROY,
             enforceSSL: true,
             autoDeleteObjects: true,
-            objectOwnership: s3.ObjectOwnership.BUCKET_OWNER_ENFORCED,
+            objectOwnership: ObjectOwnership.BUCKET_OWNER_ENFORCED,
             lifecycleRules: [{
                 expiration: Duration.days(1),
                 id: 'expirationLifeCycleRule',
             }]
         });
+
+        const targetBucket = new Bucket(this, 'targetBucket', {
+            blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+            encryption: BucketEncryption.S3_MANAGED,
+            versioned: true,
+            removalPolicy: RemovalPolicy.DESTROY,
+            enforceSSL: true,
+            autoDeleteObjects: true,
+            objectOwnership: ObjectOwnership.BUCKET_OWNER_ENFORCED,
+            lifecycleRules: [{
+                expiration: Duration.days(1),
+                id: 'expirationLifeCycleRule',
+            }]
+        });
+
+
     }
 }
