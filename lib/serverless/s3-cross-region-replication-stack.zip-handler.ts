3a6d17683fc24e8637947c48acf7098121679dcf
hkstwk <hkolvoort@gmail.com>
2024-03-14 10:31:25 +0100

    zipper working version

--- lib/serverless/s3-cross-region-replication-stack.zip-handler.ts
+++ lib/serverless/s3-cross-region-replication-stack.zip-handler.ts
@@ -1,5 +1,6 @@
-import {S3} from 'aws-sdk';
-import * as archiver from 'archiver';
+import { S3 } from 'aws-sdk';
+const archiver = require('archiver');
+const stream = require('stream');
 
 const s3 = new S3();
 
@@ -12,13 +13,10 @@ export const handler = async (event: any, context: any): Promise<void> => {
     });
 
     // Create a writable stream for archiver
-    const s3Stream = s3.upload({ Bucket: bucketName, Key: zipFileName }).on('httpUploadProgress', () => {}).promise();
+    const archiveStream = new stream.PassThrough();
 
-    // Wait for the upload to complete and obtain the writable stream
-    const stream = (await s3Stream).on('httpHeaders', () => {}).createWriteStream();
-
-    // Pipe the archive to the S3 writable stream
-    archive.pipe(stream);
+    // Pipe the archive to the PassThrough stream
+    archive.pipe(archiveStream);
 
     // Add files to the archive
     await listAndAddFilesToArchive(bucketName, archive);
@@ -26,17 +24,21 @@ export const handler = async (event: any, context: any): Promise<void> => {
     // Finalize the archiver to complete the zip file
     archive.finalize();
 
+    // Upload the zip file to S3
+    const s3Params = { Bucket: bucketName, Key: zipFileName, Body: archiveStream };
+    await s3.upload(s3Params).promise();
+
     // End the Lambda function
-    context.succeed('Zip creation successful');
+    context.succeed('Zip creation and upload successful');
 };
 
-async function listAndAddFilesToArchive(bucketName: string, archive: archiver.Archiver): Promise<void> {
+async function listAndAddFilesToArchive(bucketName: string, archive: any): Promise<void> {
     const listObjectsV2Output = await s3.listObjectsV2({ Bucket: bucketName }).promise();
 
     // @ts-ignore
     for (const content of listObjectsV2Output.Contents) {
         // Skip directories or other objects if needed
-        if (!content.Key) continue;
+
 
         // Create a stream for each S3 object
         const s3ObjectStream = s3.getObject({ Bucket: bucketName, Key: content.Key }).createReadStream();
