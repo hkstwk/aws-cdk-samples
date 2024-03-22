fc66f268360c50ba84f55fdb04adc751fddcc5cb
hkstwk <hkolvoort@gmail.com>
2024-03-22 22:41:36 +0100

    easier way to attach replication rule to Bucket construct

--- bin/aws-cdk-samples.ts
+++ bin/aws-cdk-samples.ts
@@ -4,6 +4,7 @@ import * as cdk from 'aws-cdk-lib';
 import {Ec2BasicL2Stack} from '../lib/provisioned/ec2-basic-l2-stack';
 import {Ec2BasicL1Stack} from "../lib/provisioned/ec2-basic-l1-stack";
 import {S3ReplicationStack} from "../lib/serverless/s3-replication-stack";
+import {S3ReplicationImprovedStack} from "../lib/serverless/s3-replication-improved-stack";
 
 const app = new cdk.App();
 const props = {env: {account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION}}
@@ -11,6 +12,7 @@ const props = {env: {account: process.env.CDK_DEFAULT_ACCOUNT, region: process.e
 new Ec2BasicL2Stack(app, 'Ec2BasicL2Stack', props);
 new Ec2BasicL1Stack(app, 'Ec2BasicL1Stack', props);
 new S3ReplicationStack(app, 'S3ReplicationStack', props);
+new S3ReplicationImprovedStack(app, 'S3ReplicationImprovedStack', props);
 
 app.synth();
 
--- lib/serverless/s3-replication-improved-stack.ts
+++ lib/serverless/s3-replication-improved-stack.ts
@@ -1,40 +1,28 @@
 import * as cdk from 'aws-cdk-lib';
 import {Duration, RemovalPolicy} from 'aws-cdk-lib';
 import * as iam from 'aws-cdk-lib/aws-iam';
-import * as s3 from 'aws-cdk-lib/aws-s3'; // Import s3 namespace
+import * as s3 from 'aws-cdk-lib/aws-s3';
 import {BlockPublicAccess, BucketEncryption, ObjectOwnership} from 'aws-cdk-lib/aws-s3';
 import {Construct} from 'constructs';
-import {NodejsFunction} from 'aws-cdk-lib/aws-lambda-nodejs';
-import {Runtime} from 'aws-cdk-lib/aws-lambda';
 
-export class S3ReplicationStack extends cdk.Stack {
+export class S3ReplicationImprovedStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
-        // create source bucket
-        const sourceBucket = new s3.CfnBucket(this, 'sourceBucket', {
-            versioningConfiguration: {
-                status: 'Enabled'
-            },
-            ownershipControls: {
-                rules: [{objectOwnership: s3.ObjectOwnership.BUCKET_OWNER_ENFORCED}]
-            },
-            bucketEncryption: {
-                serverSideEncryptionConfiguration: [{
-                    bucketKeyEnabled: false,
-                    serverSideEncryptionByDefault: {
-                        sseAlgorithm: "AES256"
-                    },
-                }]
-            },
-            lifecycleConfiguration: {
-                rules: [{
-                    status: 'Enabled',
-                    expirationInDays: 1
-                }]
-            }
+        // Create source bucket
+        const sourceBucket = new s3.Bucket(this, 'sourceBucket', {
+            blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+            encryption: BucketEncryption.S3_MANAGED,
+            versioned: true,
+            removalPolicy: RemovalPolicy.DESTROY,
+            enforceSSL: true,
+            autoDeleteObjects: true,
+            objectOwnership: ObjectOwnership.BUCKET_OWNER_ENFORCED,
+            lifecycleRules: [{
+                expiration: Duration.days(1),
+                id: 'expirationLifeCycleRule',
+            }]
         });
-
 
         // Create target bucket
         const targetBucket = new s3.Bucket(this, 'targetBucket', {
@@ -51,17 +39,6 @@ export class S3ReplicationStack extends cdk.Stack {
             }]
         });
 
-        // Create Lambda function
-        const zipHandler = new NodejsFunction(this, 'zip-handler', {
-            runtime: Runtime.NODEJS_20_X,
-            timeout: Duration.seconds(300),
-            memorySize: 1024,
-            environment: {
-                bucketName: targetBucket.bucketName
-            }
-        });
-        targetBucket.grantReadWrite(zipHandler);
-
         // Define IAM role for replication
         const replicationRole = new iam.Role(this, 'ReplicationRole', {
             assumedBy: new iam.ServicePrincipal('s3.amazonaws.com'),
@@ -72,7 +49,7 @@ export class S3ReplicationStack extends cdk.Stack {
                 "s3:GetReplicationConfiguration",
                 "s3:ListBucket"
             ],
-            resources: [sourceBucket.attrArn]
+            resources: [sourceBucket.bucketArn]
         }));
 
         // Attach policy for reading from the source bucket
@@ -83,7 +60,7 @@ export class S3ReplicationStack extends cdk.Stack {
                 "s3:GetObjectVersionTagging"
             ],
             resources: [
-                `${sourceBucket.attrArn}/*`,
+                `${sourceBucket.bucketArn}/*`,
             ]
         }));
 
@@ -100,7 +77,8 @@ export class S3ReplicationStack extends cdk.Stack {
         }));
 
         // Create replication rule configuration
-        sourceBucket.replicationConfiguration = {
+        const cfnBucket = sourceBucket.node.defaultChild as s3.CfnBucket;
+        cfnBucket.replicationConfiguration = {
             role: replicationRole.roleArn,
             rules: [
                 {
